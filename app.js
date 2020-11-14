86bbc6dd8fc6924a136a18d4e5f5019f3f7fe21c
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-14 21:02:50 +0700

    set the response

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  const resData = {
+    message: 'Hello world',
+    status: 200,
+  };
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(resData));
+  res.end();
   // process.exit();
 });
 
