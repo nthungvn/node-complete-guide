df0b01cf0a29cfddcd1993ad0cf77d5a96a6e93c
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-14 16:03:49 +0700

    core syntax

---
+++ play.js
@@ -0,0 +1,11 @@
+var name = 'Hung';
+var age = 28;
+var isHobbies = true;
+
+function getSummary(name, age, isHobbies) {
+  return (
+    'My name is ' + name + ', age is ' + age + ' and has hobbies ' + isHobbies
+  );
+}
+
+console.log(getSummary(name, age, isHobbies));
