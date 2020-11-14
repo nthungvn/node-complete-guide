35a2d133d05a1b57658aa79b1e89e3c9ccb02f8c
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-14 16:15:21 +0700

    arrow function

--- play.js
+++ play.js
@@ -4,10 +4,11 @@ const isHobbies = true;
 
 age = 29;
 
-function getSummary(name, age, isHobbies) {
+// const getSummary = function (name, age, isHobbies) { // Function expression
+const getSummary = (name, age, isHobbies) => {
   return (
     'My name is ' + name + ', age is ' + age + ' and has hobbies ' + isHobbies
   );
-}
+};
 
 console.log(getSummary(name, age, isHobbies));
