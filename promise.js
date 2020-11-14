68b5c2ad77808cfc9350f44cde7771e710f6b5d3
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-14 16:57:21 +0700

    confuse with lot of callback hell

--- promise.js
+++ promise.js
@@ -1,5 +1,15 @@
+const fetch = (callback) => {
+  setTimeout(() => {
+    callback('Text');
+    console.log('Done');
+  }, 1500);
+};
+
 setTimeout(() => {
   console.log('Timer is done');
+  fetch((text) => {
+    console.log(text);
+  });
 }, 1);
 
 console.log('Hello');
