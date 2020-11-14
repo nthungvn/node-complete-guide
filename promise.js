6e6e26aa4f0ddb2abe4a52cd33aca6c7793599e4
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-14 17:24:19 +0700

    promising and async await

--- promise.js
+++ promise.js
@@ -1,16 +1,50 @@
-const fetch = (callback) => {
-  setTimeout(() => {
-    callback('Text');
-    console.log('Done');
-  }, 1500);
+const fetchData = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, 1500);
+  });
 };
 
 setTimeout(() => {
   console.log('Timer is done');
-  fetch((text) => {
-    console.log(text);
-  });
+  fetchData()
+    .then((data) => {
+      console.log(data);
+      return fetchData();
+    })
+    .then((data2) => {
+      console.log(data2);
+    });
 }, 1);
 
 console.log('Hello');
 console.log('Hi');
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Text');
+  }, 2000);
+});
+
+promise
+  .then((data) => {
+    return 'Chain promise with data: ' + data;
+  })
+  .then((message) => {
+    console.log(message);
+  });
+
+const asyncAwaitCode = async () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Async - Await');
+    }, 2000);
+  });
+
+  const message = await promise;
+  console.log(await promise);
+  // console.log(message);
+};
+
+asyncAwaitCode();
