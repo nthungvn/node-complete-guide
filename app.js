6b04b7cf3da0b5cf527eab8ee8be010df40de33a
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-15 16:55:36 +0700

    use routing

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Call the next/chain middleware
+app.use('/', (req, res, next) => {
+  console.log('This always run');
+  next();
 });
 
-app.use((req, res, next) => {
+app.use('/users', (req, res, next) => {
   console.log('In another middleware');
+  res.send('<h1>List users</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In the middleware');
   res.send('<h1>Welcome to Express.js world</h1>');
 });
+
 app.listen(process.env.PORT || 3000);
