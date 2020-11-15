f5274fb28fef2d1a1690a01e5c3cba7c9fe150fb
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-15 22:10:45 +0700

    serve static assets

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(process.env.PORT || 3000);
