81adc40cfa31dea7e12d6a92454f464ee50734ec
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-15 11:34:58 +0700

    create separation routes

--- app.js
+++ app.js
@@ -1,45 +1,7 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  const { url } = req;
-  let htmlRes = '';
+const routes = require('./routes');
 
-  res.setHeader('Content-Type', 'text/html');
-
-  if (url === '/') {
-    htmlRes = `
-      <html>
-        <head>
-          <title>Node.js The Complete Guide</title>
-        </head>
-        <body>
-          <h1>Hey there, welcome to the Node.js - The complete guide</h1>
-        </body>
-      </html>
-    `;
-  }
-
-  if (url === '/users') {
-    htmlRes = `
-      <html>
-        <head>
-          <title>Node.js The Complete Guide</title>
-        </head>
-        <body>
-          <h1>Users</h1>
-          <ul>
-            <li>Hung</li>
-            <li>Tang</li>
-            <li>Hau</li>
-            <li>Tran</li>
-          </ul>
-        </body>
-      </html>
-    `;
-  }
-
-  res.write(htmlRes);
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(process.env.PORT || 3000);
