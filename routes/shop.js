372b018df282045cac386c6b73730065580cc3da
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-15 19:52:41 +0700

    a nice way of get path for cross-os

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const path = require('path');
-
 const express = require('express');
+
+const rootDir = require('../utils/path');
 
 const routes = express.Router();
 
 routes.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = routes;
