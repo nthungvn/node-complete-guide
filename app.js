da956910473031b0fdc65868c458aa0d01bad15d
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-15 16:17:58 +0700

    setup express framework

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
+const app = express();
+app.use((req, res) => {
+  res.send('OK');
+});
 
-const server = http.createServer(routes.handler);
-
+const server = http.createServer(app);
 server.listen(process.env.PORT || 3000);
