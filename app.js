f47bf20ddca58023c5cafa438de950025ac5c838
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-24 22:12:03 +0700

    serve static files

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const defaultRoutes = require('./routes/default');
 const userRoutes = require('./routes/user');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', userRoutes);
 app.use('/', defaultRoutes);
 
