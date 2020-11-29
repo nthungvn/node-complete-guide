14ab9daa2cc2e44f785bed83a307ae73e0f948fc
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-29 07:03:30 +0700

    add error controller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -14,8 +15,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' });
-});
+app.use(errorController.getNotFound);
 
 app.listen(process.env.PORT || 3000);
