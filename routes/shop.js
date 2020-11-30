9d795c1e9cfa472c31e824948966db13c505ff03
Hung Nguyen <nthung.vlvn@gmail.com>
2020-11-30 19:33:04 +0700

    delete cart items

--- routes/shop.js
+++ routes/shop.js
@@ -9,6 +9,7 @@ routes.get('/products', shopController.getProducts);
 routes.get('/products/:productId', shopController.getProduct);
 routes.get('/cart', shopController.getCart);
 routes.post('/cart', shopController.postCart);
+routes.post('/delete-cart-item', shopController.postDeleteCartItem);
 routes.get('/orders', shopController.getOrders);
 routes.get('/checkout', shopController.getCheckout);
 
