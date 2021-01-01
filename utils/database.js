f5b530f734754fef2abe64eed3babdae5a97d74b
Hung Nguyen <nthung.vlvn@gmail.com>
2021-01-01 17:23:49 +0700

    update auth to local db

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node_complete_guide', 'root', {
-  host: '34.87.158.173',
-  password: 'node-complete-guide',
+  host: 'localhost',
+  password: 'my-secret-pw',
   dialect: 'mysql',
 });
 
