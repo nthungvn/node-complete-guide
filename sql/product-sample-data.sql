fc1caabf01608eec3dfde5bb9872cdde99c98605
Hung Nguyen <nthung.vlvn@gmail.com>
2021-01-02 11:20:01 +0700

    update sample data insertion

--- sql/product-sample-data.sql
+++ sql/product-sample-data.sql
@@ -1,28 +1,36 @@
-insert into products (title, imageUrl, price, description)
+insert into products (title, imageUrl, price, description, createdAt, updatedAt)
 values (
     "iPhone SE",
     "https://cdn.tgdd.vn/Products/Images/42/76481/iphone-se-64gb-400x460.png",
     349,
-    "A small phone but has expensive price"
+    "A small phone but has expensive price",
+    '2021-01-02 03:40:55',
+    '2021-01-02 03:40:55'
   );
-insert into products (title, imageUrl, price, description)
+insert into products (title, imageUrl, price, description, createdAt, updatedAt)
 values (
     "Google Pixel 4A",
     "https://www.thegioididong.com/images/42/226666/google-pixel-4a-5g-600jpg-400x400.jpg",
     349,
-    "A great phone"
+    "A great phone",
+    '2021-01-02 03:40:55',
+    '2021-01-02 03:40:55'
   );
-insert into products (title, imageUrl, price, description)
+insert into products (title, imageUrl, price, description, createdAt, updatedAt)
 values (
     "Nokia 3.1",
     "https://cdn.tgdd.vn/Products/Images/42/169541/nokia-31-600x600.jpg",
     199,
-    "A small screen phone, it suite for people that has small hand"
+    "A small screen phone, it suite for people that has small hand",
+    '2021-01-02 03:40:55',
+    '2021-01-02 03:40:55'
   );
-insert into products (title, imageUrl, price, description)
+insert into products (title, imageUrl, price, description, createdAt, updatedAt)
 values (
     "Google Pixel 5",
     "https://www.didongmy.com/vnt_upload/product/09_2020/thumbs/600_google-pixel-5.jpg",
     569,
-    "A great phone. I will buy it"
+    "A great phone. I will buy it",
+    '2021-01-02 03:40:55',
+    '2021-01-02 03:40:55'
   );
