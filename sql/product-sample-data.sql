5e8b748cdaca20aa51e77d45a982304c3d3f441f
Hung Nguyen <nthung.vlvn@gmail.com>
2021-01-03 21:30:16 +0700

    update sample data

--- sql/product-sample-data.sql
+++ sql/product-sample-data.sql
@@ -1,36 +1,72 @@
-insert into products (title, imageUrl, price, description, createdAt, updatedAt)
-values (
+INSERT INTO products(
+    title,
+    imageurl,
+    price,
+    description,
+    createdat,
+    updatedat,
+    userid
+  )
+VALUES (
     "iPhone SE",
     "https://cdn.tgdd.vn/Products/Images/42/76481/iphone-se-64gb-400x460.png",
     349,
     "A small phone but has expensive price",
     '2021-01-02 03:40:55',
-    '2021-01-02 03:40:55'
+    '2021-01-02 03:40:55',
+    1
   );
-insert into products (title, imageUrl, price, description, createdAt, updatedAt)
-values (
+INSERT INTO products(
+    title,
+    imageurl,
+    price,
+    description,
+    createdat,
+    updatedat,
+    userid
+  )
+VALUES (
     "Google Pixel 4A",
     "https://www.thegioididong.com/images/42/226666/google-pixel-4a-5g-600jpg-400x400.jpg",
     349,
     "A great phone",
     '2021-01-02 03:40:55',
-    '2021-01-02 03:40:55'
+    '2021-01-02 03:40:55',
+    1
   );
-insert into products (title, imageUrl, price, description, createdAt, updatedAt)
-values (
+INSERT INTO products(
+    title,
+    imageurl,
+    price,
+    description,
+    createdat,
+    updatedat,
+    userid
+  )
+VALUES (
     "Nokia 3.1",
     "https://cdn.tgdd.vn/Products/Images/42/169541/nokia-31-600x600.jpg",
     199,
     "A small screen phone, it suite for people that has small hand",
     '2021-01-02 03:40:55',
-    '2021-01-02 03:40:55'
+    '2021-01-02 03:40:55',
+    1
   );
-insert into products (title, imageUrl, price, description, createdAt, updatedAt)
-values (
+INSERT INTO products(
+    title,
+    imageurl,
+    price,
+    description,
+    createdat,
+    updatedat,
+    userid
+  )
+VALUES (
     "Google Pixel 5",
     "https://www.didongmy.com/vnt_upload/product/09_2020/thumbs/600_google-pixel-5.jpg",
     569,
     "A great phone. I will buy it",
     '2021-01-02 03:40:55',
-    '2021-01-02 03:40:55'
+    '2021-01-02 03:40:55',
+    1
   );
