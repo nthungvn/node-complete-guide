b16736f14515c9e6d5ce8686ff601750fc11892b
[Miracle] Hung <hung.nguyenthai@axonactive.com>
2021-01-04 12:48:18 +0700

    create a connection to mongo

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,18 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('node-complete-guide', 'root', 'my-secret-pw', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const mongo = require('mongodb')
 
 module.exports = sequelize;
+
+const connectMongo = function() {
+  mongo
+    .connect(
+      'mongodb+srv://node-complete-guide:node-complete-guide@cluster0.oipin.mongodb.net/node-complete-guide?retryWrites=true&w=majority',
+    )
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports = connectMongo;
