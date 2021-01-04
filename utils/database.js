46771a3965493a3322f6408c1722d6ddfc1f91c2
Hung Nguyen <nthung.vlvn@gmail.com>
2021-01-04 21:54:26 +0700

    use new technology connection

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,9 @@ const MongoClient = mongo.MongoClient;
 
 const connectMongo = function () {
   return MongoClient.connect(
-    'mongodb+srv://node-complete-guide:node-complete-guide@cluster0.oipin.mongodb.net/node-complete-guide?retryWrites=true&w=majority',
+    'mongodb+srv://node-complete-guide:node-complete-guide@cluster0.oipin.mongodb.net/node-complete-guide?retryWrites=true&w=majority', {
+      useUnifiedTopology: true,
+    }
   );
 };
 
