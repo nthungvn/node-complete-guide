7db926a3144cdebcac1f853ef05561ee1f661a8b
Hung Nguyen <nthung.vlvn@gmail.com>
2021-01-19 22:38:57 +0700

    create user

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const { connectMongo } = require('./utils/database');
+const User = require('./models/user');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -15,6 +16,14 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
+  User.findById('6006fc24e95f4b367ac6b10a')
+    .then((user) => {
+      req.user = user;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   next();
 });
 app.use('/admin', adminRoutes);
@@ -24,4 +33,18 @@ app.use(errorController.getNotFound);
 connectMongo(() => {
   console.log('Connected to MongoDB');
   app.listen(process.env.PORT || 3000);
+  User.findById('6006fc24e95f4b367ac6b10a')
+    .then((user) => {
+      if (!user) {
+        const user = new User('Hung', 'hung@sample.com');
+        return user.save();
+      }
+      return user;
+    })
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
