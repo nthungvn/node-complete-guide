2edda9930928af3b056e15e52fd644112dc63981
Hung Nguyen <nthung.vlvn@gmail.com>
2021-01-27 20:26:51 +0700

    update a product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,13 +1,16 @@
+const { ObjectId } = require('mongodb');
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.find().exec().then((products) => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/admin/products',
+  Product.find()
+    .exec()
+    .then((products) => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/admin/products',
+      });
     });
-  });
 };
 
 exports.getAddProduct = (req, res, next) => {
@@ -26,33 +29,26 @@ exports.getEditProduct = (req, res, next) => {
 
   const { productId } = req.params;
 
-  Product.findById(productId)
-    .then((product) => {
-      if (!product) {
-        return res.redirect('/');
-      }
+  Product.findById(productId).then((product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
 
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product,
-      });
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
     });
+  });
 };
 
 exports.postEditProduct = (req, res, next) => {
   const { productId, title, imageUrl, price, description } = req.body;
-  const updatedProduct = new Product(
-    title,
-    imageUrl,
-    price,
-    description,
-    productId,
-    req.user._id,
-  );
-  updatedProduct
-    .save()
+  Product.findOneAndUpdate(
+    { _id: new ObjectId(productId) },
+    { title, imageUrl, price, description },
+  )
     .then(() => {
       res.redirect('/admin/products');
     })
@@ -76,9 +72,17 @@ exports.postDeleteProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, price, description } = req.body;
   console.log(req.user);
-  const product = new Product(title, imageUrl, price, description, null, req.user._id);
+  const product = new Product(
+    title,
+    imageUrl,
+    price,
+    description,
+    null,
+    req.user._id,
+  );
 
-  product.save()
+  product
+    .save()
     .then((result) => {
       console.log(result);
       res.redirect('/admin/products');
