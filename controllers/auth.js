463f746f9d83aac57722d3ab0a84fcd7b2f385f2
Hung Nguyen <nthung.vlvn@gmail.com>
2021-01-31 10:30:56 +0700

    add login form

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,13 @@
 const getLogin = (req, res, next) => {
-  res.json({ message: 'OK' });
+  res.render('auth/login', {
+    pageTitle: 'Login',
+    path: '/login'
+  })
 };
 
 const postLogin = (req, res, next) => {
+  const {username, password} = req.body;
+  console.log(username, password);
   res.json({ message: 'OK' });
 };
 
