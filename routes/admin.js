3ef3a19e9df6c2868504d63985052ebbcb9f51ec
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-07 22:46:06 +0700

    project the routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,19 @@
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const authGuard = require('../middleware/authGuard');
 
 const routes = express.Router();
 
 // /admin/add-product => GET
-routes.get('/add-product', adminController.getAddProduct);
+routes.get('/add-product', authGuard, adminController.getAddProduct);
 // /admin/add-product => POST
-routes.post('/add-product', adminController.postAddProduct);
-routes.get('/edit-product/:productId', adminController.getEditProduct);
-routes.post('/edit-product', adminController.postEditProduct);
-routes.post('/delete-product', adminController.postDeleteProduct);
+routes.post('/add-product', authGuard, adminController.postAddProduct);
+routes.get('/edit-product/:productId', authGuard, adminController.getEditProduct);
+routes.post('/edit-product', authGuard, adminController.postEditProduct);
+routes.post('/delete-product', authGuard, adminController.postDeleteProduct);
 
 // /admin/products => GET
-routes.get('/products', adminController.getProducts);
+routes.get('/products', authGuard, adminController.getProducts);
 
 module.exports = routes;
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,18 @@
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const authGuard = require('../middleware/authGuard');
 
 const routes = express.Router();
 
 routes.get('/', shopController.getIndex);
 routes.get('/products', shopController.getProducts);
 routes.get('/products/:productId', shopController.getProduct);
-routes.get('/cart', shopController.getCart);
-routes.post('/cart', shopController.postCart);
-routes.post('/delete-cart-item', shopController.postDeleteCartItem);
-routes.get('/orders', shopController.getOrders);
-routes.get('/checkout', shopController.getCheckout);
-routes.post('/create-order', shopController.postOrder);
+routes.get('/cart', authGuard, shopController.getCart);
+routes.post('/cart', authGuard, shopController.postCart);
+routes.post('/delete-cart-item', authGuard, shopController.postDeleteCartItem);
+routes.get('/orders', authGuard, shopController.getOrders);
+routes.get('/checkout', authGuard, shopController.getCheckout);
+routes.post('/create-order', authGuard, shopController.postOrder);
 
 module.exports = routes;
