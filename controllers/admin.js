c54504ca76eacd6f4096c262da4b503e34dec0a6
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-12 14:18:35 +0700

    move function position

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,6 +30,38 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
+exports.postAddProduct = (req, res, next) => {
+  const { title, imageUrl, price, description } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      errorMessage: errors.array()[0].msg,
+      hasError: true,
+      product: { title, imageUrl, price, description },
+      validationErrors: errors.array(),
+    });
+  }
+
+  Product.create({
+    title,
+    imageUrl,
+    price,
+    description,
+    userId: req.session.user,
+  })
+    .then((result) => {
+      res.redirect('/admin/products');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
@@ -96,38 +128,6 @@ exports.postDeleteProduct = (req, res, next) => {
 
   Product.deleteOne({ _id: productId, userId: req.user._id.toString() })
     .then(() => {
-      res.redirect('/admin/products');
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-exports.postAddProduct = (req, res, next) => {
-  const { title, imageUrl, price, description } = req.body;
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(422).render('admin/edit-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      editing: false,
-      errorMessage: errors.array()[0].msg,
-      hasError: true,
-      product: { title, imageUrl, price, description },
-      validationErrors: errors.array(),
-    });
-  }
-
-  Product.create({
-    title,
-    imageUrl,
-    price,
-    description,
-    userId: req.session.user,
-  })
-    .then((result) => {
       res.redirect('/admin/products');
     })
     .catch((error) => {
