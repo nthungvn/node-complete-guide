91eacc272a32abfb0df0aaf5a03e85b1f667cb81
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-13 13:39:14 +0700

    read the whole file and response to user

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const Order = require('../models/order');
@@ -137,7 +137,7 @@ exports.getInvoice = (req, res, next) => {
   const { orderId } = req.params;
   Order.findOne({ _id: orderId, userId: req.session.user._id }).then(
     (order) => {
-      if (order) {
+      if (!order) {
         return res.redirect('/404');
       }
       const invoicePath = path.resolve(
@@ -145,10 +145,22 @@ exports.getInvoice = (req, res, next) => {
         'invoices',
         `invoice-${orderId}.pdf`,
       );
-      const invoiceFile = fs.createReadStream(invoicePath);
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', `inline; filename=invoice-${orderId}.pdf`);
-      invoiceFile.pipe(res);
+      fs.readFile(invoicePath)
+        .then((file) => {
+          res.setHeader('Content-Type', 'application/pdf');
+          res.setHeader(
+            'Content-Disposition',
+            `inline; filename=invoice-${orderId}.pdf`,
+          );
+          res.send(file);
+        })
+        .catch((error) => {
+          const errorNext = new Error(error);
+          errorNext.httpStatusCode = 500;
+          next(errorNext);
+        });
+      // invoiceFile.pipe(res);
+      // const invoiceFile = fs.createReadStream(invoicePath);
     },
   );
 };
