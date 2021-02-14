6717c6ebe8b8fded342fb10e4b54644497178a03
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-14 17:15:54 +0700

    add the json parser

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const postRoutes = require('./routes/post');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use('/feed', postRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
