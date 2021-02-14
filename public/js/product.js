3485b3c739309672de8b41529e90e2812bd815c8
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-14 07:46:08 +0700

    send request to server for deleting product and remove product on client

--- public/js/product.js
+++ public/js/product.js
@@ -1,6 +1,19 @@
 function deleteProduct(btn) {
   const csrfToken = btn.parentNode.querySelector('[name="_csrf"]').value;
   const productId = btn.parentNode.querySelector('[name="productId"]').value;
+  const productCard = btn.closest('article');
 
-  console.log(csrfToken, productId);
+  fetch('/admin/products/' + productId, {
+    method: 'DELETE',
+    headers: {
+      'csrf-token': csrfToken,
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      productCard.parentNode.removeChild(productCard);
+    })
+    .catch((error) => console.log(error));
 }
