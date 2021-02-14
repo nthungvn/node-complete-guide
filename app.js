9729432872e08ba594f9280ead35630733e5e319
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-14 18:53:58 +0700

    rename file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const postRoutes = require('./routes/post');
+const feedRoutes = require('./routes/feed');
 const cors = require('./middlewares/cors');;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/feed', postRoutes);
+app.use('/feed', feedRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("App started");
