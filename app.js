7b1eed1fbb852bc9c31c4b70cff8ac13633bf528
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-15 08:50:27 +0700

    connect mongodb

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
-const cors = require('./middlewares/cors');;
+const cors = require('./middlewares/cors');
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.oipin.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
 
 const app = express();
 
@@ -10,6 +13,12 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/feed', feedRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("App started");
-});
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('Mongo connected');
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
