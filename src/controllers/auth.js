dd60102d310857683159bd52882ec03b447312f9
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-15 21:38:15 +0700

    add login routes

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
-exports.signup = (req, res, next) => {
+exports.postSignup = (req, res, next) => {
   const { name, email, password } = req.body;
 
   User.findOne({ email: email })
@@ -27,7 +27,36 @@ exports.signup = (req, res, next) => {
         user: {
           name: result.name,
           email: result.email,
-          token: 'result.password'
+          token: 'result.password',
+        },
+      });
+    })
+    .catch((error) => next(error));
+};
+
+exports.postLogin = (req, res, next) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error('User not found');
+        error.statusCode = 404;
+        throw error;
+      }
+      return bcrypt.compare(password, user.password);
+    })
+    .then((doMatch) => {
+      if (!doMatch) {
+        const error = new Error('User or password was wrong');
+        error.statusCode = 401;
+        throw error;
+      }
+      res.status(200).json({
+        message: 'OK',
+        user: {
+          email: email,
+          token: 'waiting',
         },
       });
     })
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ const Router = require('express').Router;
 const authController = require('../controllers/auth');
 
 const router = Router();
-router.post('/signup', authController.signup);
+router.post('/signup', authController.postSignup);
+router.post('/login', authController.postLogin);
 
 module.exports = router;
