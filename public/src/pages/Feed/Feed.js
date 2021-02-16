4b6546edaba4f3d2e8f56af37128583bab9f7276
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-16 22:37:23 +0700

    clean up

--- public/src/pages/Feed/Feed.js
+++ public/src/pages/Feed/Feed.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import { io as openSocket } from 'socket.io-client';
 
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
@@ -38,18 +37,6 @@ class Feed extends Component {
         this.setState({ status: resData.status });
       })
       .catch(this.catchError);
-    const socket = openSocket('http://localhost:8080', {
-      transports: ['websocket'],
-    });
-    socket.on('posts', data => {
-      if (data.action === 'create') {
-        this.addPost(data.post);
-      } else if (data.action === 'delete') {
-        this.deletePost(data.postId)
-      } else if (data.action === 'update') {
-        this.updatePost(data.post);
-      }
-    });
     this.loadPosts();
   }
 
@@ -89,40 +76,6 @@ class Feed extends Component {
       })
       .catch(this.catchError);
   };
-
-  addPost = post => {
-    this.setState(prevState => {
-      const updatedPosts = [...prevState.posts];
-      if (prevState.postPage === 1) {
-        if (prevState.posts.length >= 3) {
-          updatedPosts.pop();
-        }
-        updatedPosts.unshift(post);
-      }
-      return {
-        posts: updatedPosts,
-        totalPosts: prevState.totalPosts + 1
-      };
-      });
-
-  };
-
-  updatePost = post => {
-    this.setState(prevState => {
-      const updatedPosts = [...prevState.posts];
-      const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
-      if (updatedPostIndex > -1) {
-        updatedPosts[updatedPostIndex] = post;
-      }
-      return {
-        posts: updatedPosts
-      };
-    });
-  }
-
-  deletePost = postId => {
-    this.loadPosts();
-  }
 
   statusUpdateHandler = event => {
     event.preventDefault();
