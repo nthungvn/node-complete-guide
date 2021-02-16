73a624eab420d16e17e7d0880d21c514a7099ae0
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-16 21:56:39 +0700

    update logic after adding post

--- public/src/pages/Feed/Feed.js
+++ public/src/pages/Feed/Feed.js
@@ -89,11 +89,20 @@ class Feed extends Component {
   };
 
   addPost = post => {
-    if (this.state.posts.length < 3) {
-      this.setState({
-        posts: this.state.posts.concat(post),
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 3) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
       });
-    }
+
   };
 
   deletePost = postId => {
--- src/socket.js
+++ src/socket.js
@@ -3,13 +3,14 @@ let _io;
 module.exports = {
   init: (httpServer, options) => {
     const opts = options || {};
-    if (!_io) {
-      _io = require('socket.io')(httpServer, opts);
-    }
+    _io = require('socket.io')(httpServer, opts);
     return _io;
   },
 
   io: () => {
+    if (!_io) {
+      throw new Error('Socket did not initialize');
+    }
     return _io;
   },
 };
