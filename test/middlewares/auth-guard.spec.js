75db28848c187fc6cab0adc8d65830fe78659c50
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-06 21:40:53 +0700

    organize multiple tests

--- test/middlewares/auth-guard.spec.js
+++ test/middlewares/auth-guard.spec.js
@@ -1,14 +1,30 @@
-const { expect } =  require('chai');
+const { expect } = require('chai');
+const { describe } = require('mocha');
 
 const isAuthMiddleware = require('../../src/middlewares/auth-guard');
 
-it('should throw an error if no authorization header is present', async () => {
-  const req = {
-    headers: {},
-  };
-  try {
-    await isAuthMiddleware(req, {}, () => {})
-  } catch (error) {
-    expect(error.message).to.equal('Not Authenticated');
-  }
+describe('Auth guard middleware', () => {
+  it('should throw an error if no authorization header is present', async () => {
+    const req = {
+      headers: {},
+    };
+    try {
+      await isAuthMiddleware(req, {}, () => {});
+    } catch (error) {
+      expect(error.message).to.equal('Not Authenticated');
+    }
+  });
+
+  it('should throw an error if the authorization header is only one string', async () => {
+    const req = {
+      headers: {
+        authorization: 'Bearer',
+      },
+    };
+    try {
+      await isAuthMiddleware(req, {}, () => {});
+    } catch (error) {
+      expect(error.message).to.equal('Not Authenticated');
+    }
+  });
 });
