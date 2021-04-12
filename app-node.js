cda9096693b1f767e3b991ce6c7c6d7d42e66552
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-12 16:11:20 +0700

    rename

--- app-node.js
+++ app-node.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const todoRoutes = require('./routes/todos');
+const todoRoutes = require('./routes/todos-node');
 
 const app = express();
 app.use(express.json());
