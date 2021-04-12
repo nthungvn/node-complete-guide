cba3ad4b6f1966f650e9fc8bf3927184b5b7cae1
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-12 23:40:02 +0700

    downgrade version to v0.13.0

--- helpers/db_client.ts
+++ helpers/db_client.ts
@@ -1,14 +1,31 @@
-import { MongoClient } from 'https://deno.land/x/mongo@v0.22.0/mod.ts';
-import { Database } from 'https://deno.land/x/mongo@v0.22.0/src/database.ts';
-
+import { MongoClient, Database } from 'https://deno.land/x/mongo@v0.13.0/mod.ts';
 
 let db: Database;
 export async function connect() {
   const client = new MongoClient();
-  await client.connect('mongodb+srv://node-complete-guide:node-complete-guide@cluster0.oipin.mongodb.net/node-complete-guide-deno?retryWrites=true&w=majority');
-  db = client.database('node-complete-guide-deno');
+  await client.connectWithUri('mongodb+srv://node-complete-guide:node-complete-guide@cluster0.oipin.mongodb.net/?retryWrites=true&w=majority');
+  db = client.database("node-complete-guide-deno");
+  // db = await client.connect({
+  //   db: 'node-complete-guide-deno',
+  //   tls: true,
+  //   servers: [
+  //     {
+  //       host: 'cluster0.oipin.mongodb.net',
+  //       port: 27017,
+  //     },
+  //   ],
+  //   credential: {
+  //     username: 'node-complete-guide',
+  //     password: 'node-complete-guide',
+  //     db: 'admin',
+  //     mechanism: 'SCRAM-SHA-1',
+  //   },
+  // });
 }
 
 export function getDb() {
+  if (!db) {
+    throw new Error('Db was not established');
+  }
   return db;
 }
