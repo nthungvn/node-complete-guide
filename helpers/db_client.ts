475827cdb48272e227ada5b2065e4258cfe4e2c3
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-13 00:27:58 +0700

    upgrade version to v0.22.0

--- helpers/db_client.ts
+++ helpers/db_client.ts
@@ -1,26 +1,26 @@
-import { MongoClient, Database } from 'https://deno.land/x/mongo@v0.13.0/mod.ts';
+import { MongoClient } from 'https://deno.land/x/mongo@v0.22.0/mod.ts';
+import { Database } from 'https://deno.land/x/mongo@v0.22.0/src/database.ts';
 
 let db: Database;
+
 export async function connect() {
   const client = new MongoClient();
-  await client.connectWithUri('mongodb+srv://node-complete-guide:node-complete-guide@cluster0.oipin.mongodb.net/?retryWrites=true&w=majority');
-  db = client.database("node-complete-guide-deno");
-  // db = await client.connect({
-  //   db: 'node-complete-guide-deno',
-  //   tls: true,
-  //   servers: [
-  //     {
-  //       host: 'cluster0.oipin.mongodb.net',
-  //       port: 27017,
-  //     },
-  //   ],
-  //   credential: {
-  //     username: 'node-complete-guide',
-  //     password: 'node-complete-guide',
-  //     db: 'admin',
-  //     mechanism: 'SCRAM-SHA-1',
-  //   },
-  // });
+  db = await client.connect({
+    db: 'node-complete-guide-deno',
+    tls: true,
+    servers: [
+      {
+        host: 'cluster0-shard-00-00.oipin.mongodb.net',
+        port: 27017,
+      },
+    ],
+    credential: {
+      username: 'node-complete-guide',
+      password: 'node-complete-guide',
+      db: 'admin',
+      mechanism: 'SCRAM-SHA-1',
+    },
+  });
 }
 
 export function getDb() {
