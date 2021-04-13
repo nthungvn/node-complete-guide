97578c391d770ae70d14cd4923090f0c06fbbb0c
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-13 11:19:06 +0700

    can create a totos

--- routes/todos-deno.ts
+++ routes/todos-deno.ts
@@ -1,63 +1,82 @@
 import { Router } from 'https://deno.land/x/oak@v6.5.0/mod.ts';
+import { ObjectID } from 'https://deno.land/x/mongo@v0.22.0/bson/mod.ts';
+
 import { getDb } from '../helpers/db_client.ts';
 
 interface Todo {
-  id: string;
+  id?: string;
   text: string;
 }
 
 const router = new Router();
 
-let todos: Todo[] = [];
-
-router.get('/todos', (ctx, _) => {
-  ctx.response.body = { todos: todos };
+router.get('/todos', async (ctx, _) => {
+  try {
+    const todos = await getDb().collection('todos').find();
+    ctx.response.body = {
+      todos: todos.map((todo: any) => {
+        return { id: todo._id.toString(), text: todo.text };
+      }),
+    };
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get('/todos/:todoId', (ctx, _) => {
-  const todo = todos.find((todo) => todo.id === ctx.params.todoId);
-  if (todo) {
-    ctx.response.body = { todo: todo };
-    return;
+router.get('/todos/:todoId', async (ctx, _) => {
+  try {
+    const todo = await getDb()
+      .collection('todos')
+      .findOne({ _id: new ObjectID(ctx.params.todoId) });
+    if (todo) {
+      ctx.response.body = { todo: todo };
+      return;
+    }
+    ctx.response.body = { message: 'Todo not found' };
+    ctx.response.status = 404;
+  } catch (error) {
+    console.log(error);
   }
-  ctx.response.body = { message: 'Todo not found' };
-  ctx.response.status = 404;
 });
 
 router.post('/todos', async (ctx, _) => {
-  const data = await ctx.request.body().value;
-  const todo: Todo = {
-    id: Date.now().toString(),
-    text: data.text,
-  };
-  todos.push(todo);
-  ctx.response.body = { todo: todo };
-  ctx.response.status = 201;
+  try {
+    const data = await ctx.request.body().value;
+    const todo: Todo = {
+      text: data.text,
+    };
+    const createdId = await getDb().collection('todos').insertOne(todo);
+    todo.id = createdId.toString();
+    ctx.response.body = { todo: { id: createdId.toString(), text: todo.text } };
+    ctx.response.status = 201;
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.put('/todos/:todoId', async (ctx, _) => {
-  const data = await ctx.request.body().value;
-  const id = ctx.params.todoId!;
-  const todoIndex = todos.findIndex((todo) => todo.id === id);
-  if (todoIndex !== -1) {
-    todos[todoIndex] = { id: id, text: data.text };
-    ctx.response.body = { message: 'Updated', todos: todos };
-    return;
-  }
-  ctx.response.body = { message: 'Todo not found' };
-  ctx.response.status = 404;
-});
+// router.put('/todos/:todoId', async (ctx, _) => {
+//   const data = await ctx.request.body().value;
+//   const id = ctx.params.todoId!;
+//   const todoIndex = todos.findIndex((todo) => todo.id === id);
+//   if (todoIndex !== -1) {
+//     todos[todoIndex] = { id: id, text: data.text };
+//     ctx.response.body = { message: 'Updated', todos: todos };
+//     return;
+//   }
+//   ctx.response.body = { message: 'Todo not found' };
+//   ctx.response.status = 404;
+// });
 
-router.delete('/todos/:todoId', (ctx, _) => {
-  const todoIndex = todos.findIndex((todo) => todo.id === ctx.params.todoId);
-  if (todoIndex !== -1) {
-    console.log(todoIndex);
-    todos.splice(todoIndex, 1);
-    ctx.response.body = { message: 'Deleted', todos: todos };
-    return;
-  }
-  ctx.response.body = { message: 'Todo not found' };
-  ctx.response.status = 404;
-});
+// router.delete('/todos/:todoId', (ctx, _) => {
+//   const todoIndex = todos.findIndex((todo) => todo.id === ctx.params.todoId);
+//   if (todoIndex !== -1) {
+//     console.log(todoIndex);
+//     todos.splice(todoIndex, 1);
+//     ctx.response.body = { message: 'Deleted', todos: todos };
+//     return;
+//   }
+//   ctx.response.body = { message: 'Todo not found' };
+//   ctx.response.status = 404;
+// });
 
 export default router;
